aea4e97a3ff88847eb02a7982d390ab645911006
Rida Malik <internalprogression@gmail.com>
2025-08-28 11:58:07 +0500

    Added notes routes

---
+++ routes/notes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const { getNotes, addNote, deleteNote } = require("../controllers/notesController");
+
+// Routes
+router.get("/", getNotes);
+router.post("/", addNote);
+router.delete("/:id", deleteNote);
+
+module.exports = router;
